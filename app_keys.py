0f3021d305969f8014dbbc33580fd3f986276972
Humad <humad@live.com>
2017-08-11 22:54:00 +0500

    removed emotion recognition files and moved them to a different repo

---
+++ app_keys.py
@@ -0,0 +1,14 @@
+def get_consumer_key():
+    return "A6NMtrD6FrufJXzsc35tE8RPt"
+
+def get_consumer_secret():
+    return "2Sj1iwespbG5cPKPUNiCVfSYvElJzKlbbExP1d6G6Sc5mov9Gy"
+
+def get_access_token():
+    return "787158653941932033-UEOeiqTMpcU9imecVzBpV49AymVPloD"
+
+def get_access_secret():
+    return "YUmCZobFlwMWvEAeBAT2j6UnDdJMCuNFaXe9nLfaYeS4y"
+
+def get_nyt_key():
+    return "ab2fa0e0074245feb486826c72e594bc"
